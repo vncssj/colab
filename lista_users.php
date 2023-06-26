ba34adf89ca52a1b42d5127a01327dc8e27f47ac
Vinícius <vncssj@gmail.com>
2023-06-26 18:56:07 -0300

    Tela de listagem de usuarios

---
+++ lista_users.php
@@ -0,0 +1,33 @@
+<?php
+require 'conexao.php';
+
+$sql = "SELECT * FROM usuarios";
+$usuarios = $conn->exec($sql);
+$usuarios = $usuarios->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Usuários</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <td>nome</td>
+            <td>login</td>
+        </tr>
+        <?php foreach ($usuarios as $user) : ?>
+            <tr>
+                <td><?= $user->nome ?></td>
+                <td><?= $user->login ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
